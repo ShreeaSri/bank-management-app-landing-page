5a0f239df18faf01bce1d12e5987bdab167ae250
Shreea Sri <shreeasri@gmail.com>
2023-09-01 20:12:09 +0530

    implemented smooth scrolling to feature section

--- script.js
+++ script.js
@@ -28,3 +28,29 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//implementing smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport ',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //scrolling old way
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //modern way scrolling
+  // section1.scrollIntoView({ behavior: 'smooth' });
+});
